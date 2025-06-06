35df368f3386919f02bef20b76a343ae2b95da77
borisbugaev <zippityzop@protonmail.com>
2025-06-05 22:45:15 -0400

    1:2

---
+++ pseud.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <chrono>
+
+/*
+pseudo-intellectually solving sudoku
+see definatrics.txt
+*/
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}

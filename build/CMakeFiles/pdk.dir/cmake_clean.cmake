f0f69f31284e233abc0b616eb9dba5a593216129
borisbugaev <zippityzop@protonmail.com>
2025-06-22 12:19:44 -0400

    1:34

---
+++ build/CMakeFiles/pdk.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdk.dir/binit.cpp.obj"
+  "CMakeFiles/pdk.dir/binit.cpp.obj.d"
+  "CMakeFiles/pdk.dir/bparse.cpp.obj"
+  "CMakeFiles/pdk.dir/bparse.cpp.obj.d"
+  "CMakeFiles/pdk.dir/draw.cpp.obj"
+  "CMakeFiles/pdk.dir/draw.cpp.obj.d"
+  "CMakeFiles/pdk.dir/eval_utils.cpp.obj"
+  "CMakeFiles/pdk.dir/eval_utils.cpp.obj.d"
+  "CMakeFiles/pdk.dir/kmd.cpp.obj"
+  "CMakeFiles/pdk.dir/kmd.cpp.obj.d"
+  "CMakeFiles/pdk.dir/main.cpp.obj"
+  "CMakeFiles/pdk.dir/main.cpp.obj.d"
+  "CMakeFiles/pdk.dir/solver_caller.cpp.obj"
+  "CMakeFiles/pdk.dir/solver_caller.cpp.obj.d"
+  "libpdk.dll.a"
+  "pdk.exe"
+  "pdk.exe.manifest"
+  "pdk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pdk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
